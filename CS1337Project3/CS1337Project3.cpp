4ffcf0f6fcd70bea99bb8bd651408c5da3c9c668
JamesRobRob <Jamespr.0312@gmail.com>
2016-10-26 16:18:34 -0500

    Not working completely :^(

--- CS1337Project3/CS1337Project3.cpp
+++ CS1337Project3/CS1337Project3.cpp
@@ -4,8 +4,10 @@
 using namespace std;
 
 int checkOdd();
-void createSquare(int **);
-void writeToFile(int **);
+void createSquare(int **, int);
+void writeToFile(int **, int);
+
+
 
 int main() {
 	while (true) {
@@ -18,6 +20,15 @@ int main() {
 
 		if (magicalNumber == 0) {
 			break;
+		}
+
+		createSquare(magicalArray, magicalNumber);
+
+		for (int i = 0; i < magicalNumber; i++) {
+			for (int x = 0; x < magicalNumber; x++) {
+				cout << magicalArray[i][x] << " ";
+			}
+			cout << endl;
 		}
 	}
 }
@@ -38,6 +49,38 @@ int checkOdd() {
 	}
 }
 
-void createSquare(int **arr) {
+void createSquare(int **arr, int arrLength) {
+	int x = arrLength / 2 + 1;
+	int y = 0;
 
+	arr[x][y] = 1;
+
+	for (int i = 0; i < arrLength; i++) {
+		for (int x = 0; x < arrLength; x++) {
+			arr[i][x] = -1;
+		}
+	}
+	
+	for (int i = 1; i <= arrLength * arrLength; i++) {
+		int newX = x + 1;
+		int newY = y + 1;
+
+		if (newX > arrLength - 1) {
+			newX = 0;
+		}
+		if (newY > arrLength - 1) {
+			newY = 0;
+		}
+		if (arr[newX][newY] == -1) {
+			arr[newX][newY] = i;
+
+			continue;
+		}
+
+		newY = y - 1;
+		arr[x][newY] = i;
+
+		x = newX;
+		y = newY;
+	}
 }
